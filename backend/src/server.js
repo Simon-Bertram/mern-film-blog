024d0465edb280dde3afa18cec52db669330c287
simon-bertram <simonbertram@hotmail.com>
2023-10-26 12:47:13 +0100

    Add dummy data and upvote route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,46 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from "cors";
 
 const app = express();
 
+let reviewsInfo = [{
+  'name': 'killers-of-the-flower-moon',
+  'upvotes': 0,
+}, {
+  'name': 'saw-x',
+  'upvotes': 0,
+}, {
+  'name': 'a-haunting-in-venice',
+  'upvotes': 0,
+}, {
+  'name': 'the-creator',
+  'upvotes': 0,
+}];
+
+// Allow the frontend to make requests from a different origin
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.put('/api/reviews/:name/upvote', (req, res) => {
+  const { name } = req.params;
+  const review = reviewsInfo.find((review) => review.name === name);
+  if (review) {
+    review.upvotes += 1;
+    res.status(200).send(`The ${name} review now has ${review.upvotes} upvotes!`);
+  } else {
+    res.status(404).send("Review not found");
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
